0bdf61b9f5dde06ddf7d5662de2b1c2c737e4fbb
Lorentz Ponelis <lorentz.ponelis@datavant.com>
2024-09-11 17:03:38 -0400

    fix sql schema

--- cicd/poc/database/tables/test_table.sql
+++ cicd/poc/database/tables/test_table.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 --changeset lbpoc:countries_table runOnChange:false context:INITIAL_SYNC labels:INITIAL_SYNC
 --comment Initial changeset for countries_table
-  create table com.countries 
+  create table poc.countries 
    (	COUNTRY_ID text not null,
 	COUNTRY_NAME text,
 	REGION_ID int,
