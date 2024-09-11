27631ce55aa3571eef00e5a6f0fe65988e539bfd
Lorentz Ponelis <lorentz.ponelis@datavant.com>
2024-09-11 10:17:11 -0400

    add poc database lb configs

---
+++ cicd/poc/database/tables/test_table.sql
@@ -0,0 +1,9 @@
+--liquibase formatted sql
+--changeset lbpoc:countries_table runOnChange:false context:INITIAL_SYNC labels:INITIAL_SYNC
+--comment Initial changeset for countries_table
+  create table com.countries 
+   (	COUNTRY_ID text not null,
+	COUNTRY_NAME text,
+	REGION_ID int,
+	CONSTRAINT COUNTRY_C_ID_PK PRIMARY KEY (COUNTRY_ID)
+   );
